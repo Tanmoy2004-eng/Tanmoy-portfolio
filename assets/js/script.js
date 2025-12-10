0e185b54966b7ebf40cf59f66a06ca3113c3e4c9
Tanmoy2004-eng <tanmoymaitit@gmail.com>
2025-12-10 23:15:28 +0530

    Update Tawk.to chat script source URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -198,7 +198,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         s0 = document.getElementsByTagName("script")[0];
 
     s1.async = true;
-    s1.src = 'https://tawk.to/chat/6939a06b5947ce197a70cc07/1jc4k55gh';  // Your chat receiver
+    s1.src = 'https://tawk.to/chat/6939b0619b54b019818bba0f/1jc4li04b';  // Your chat receiver
 
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
